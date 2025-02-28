f70a97d99e03f015323c1eb5b353fd5edb4292e3
MissKellyCosta <kelly.mayara9@gmail.com>
2025-02-28 17:45:20 -0300

    Current Version

--- practice.py
+++ practice.py
@@ -49,3 +49,29 @@ while True:
         break
 
 estoquepao
+
+while True:
+    list_produto = navegadorpa.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div/main/div/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/a")
+    for info in list_produto:
+        info.click()
+        slp()
+        estoquepao = dict()
+        nome = navegadorpa.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/main/div[2]/div/div[1]/div[2]")
+        all_nomes = nome.find_elements(By.CLASS_NAME, "H1Styled-sc-18mg5yw-0")
+        for n in all_nomes:
+            print(n.text)
+        valor = navegadorpa.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/main/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div[1]/div/div/div[1]")
+        all_valores = valor.find_elements(By.CLASS_NAME, "TextComponent-sc-w4b5ef-0")
+        for v in all_valores:
+            print(v.text)        
+        '''estoquepao['Nome: '] = n.text
+        estoquepao['Valor: '] = v.text
+        print(estoquepao)'''
+    navegadorpa.back()
+    try:
+        wait = WebDriverWait(navegadorpa, 5)
+        wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div[2]/div/main/div/div[3]/div/div[2]/div[2]/div[1]/div/div[3]/div[1]/a")))
+    except:
+        break
+
+estoquepao
